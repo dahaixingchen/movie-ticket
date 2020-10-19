998e8f25b85e3b18dbf231f8bb5e3d4b6376e7ed
程飞 <chengfei@newland.com.cn>
2020-10-19 20:03:45 +0800

    数据跨域问题

--- src/main/java/com/fei/movieticket/controller/MovieTicketController.java
+++ src/main/java/com/fei/movieticket/controller/MovieTicketController.java
@@ -8,6 +8,7 @@ import com.fei.movieticket.vo.TicketVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +26,7 @@ import java.util.List;
 @Api("电影票")
 @RestController
 @RequestMapping("/movie")
+@CrossOrigin
 public class MovieTicketController {
 
     @Autowired
