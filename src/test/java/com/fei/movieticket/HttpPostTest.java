51ef453ec6e0ab32a5f26bbfa0906677464a5f47
程飞 <chengfei@newland.com.cn>
2020-10-21 20:29:18 +0800

    post请求的数据读取

--- src/test/java/com/fei/movieticket/HttpPostTest.java
+++ src/test/java/com/fei/movieticket/HttpPostTest.java
@@ -1,21 +1,22 @@
 package com.fei.movieticket;
 
 import com.fei.movieticket.util.HttpConstant;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
 
-import javax.sound.midi.Soundbank;
 import java.io.IOException;
 
 /**
@@ -44,11 +45,11 @@ public class HttpPostTest {
     //带json串的post请求
     @Test
     public void test2() throws IOException {
-        CloseableHttpClient build = HttpClientBuilder.create().build();
+        HttpClient build = HttpClientBuilder.create().build();
         HttpPost httpPost = new HttpPost("http://10.3.0.52:8081/movie/ticket");
         httpPost.setHeader("Host","10.3.0.52:8081");
 
-        StringEntity stringEntity = new StringEntity("{{\"cinema\":\"猫眼\"}}",HttpConstant.UTF8_ENCODE);
+        StringEntity stringEntity = new StringEntity("{\"cinema\":\"猫眼\"}",HttpConstant.UTF8_ENCODE);
         stringEntity.setContentEncoding(HttpConstant.UTF8_ENCODE);
         stringEntity.setContentType(HttpConstant.APPLICATION_JSON);
         httpPost.setEntity(stringEntity);
@@ -58,5 +59,22 @@ public class HttpPostTest {
             String s = EntityUtils.toString(entity);
             System.out.println(s);
         }
+    }
+
+    @Test
+    public void test3() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\"cinema\":\"猫眼\"}");
+        Request request = new Request.Builder()
+                .url("http://10.3.0.52:8081/movie/ticket")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+        Response response = client.newCall(request).execute();
+        ResponseBody body1 = response.body();
+        String string = body1.string();
+        System.out.println(string);
     }
 }
