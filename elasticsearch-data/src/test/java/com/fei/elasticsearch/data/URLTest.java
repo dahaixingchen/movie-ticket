9983cb01d1774a903da9f28a23521e26be690d45
程飞 <chengfei@newland.com.cn>
2020-10-30 22:21:41 +0800

    从新解析HTML类型

--- elasticsearch-data/src/test/java/com/fei/elasticsearch/data/URLTest.java
+++ elasticsearch-data/src/test/java/com/fei/elasticsearch/data/URLTest.java
@@ -17,15 +17,21 @@ import java.io.IOException;
 public class URLTest {
     public static void main(String[] args) throws IOException {
         File file = new File("C:\\movie\\movie.txt");
+        File file1 = new File("C:\\movie\\not_check_movie.txt");
         FileWriter out = new FileWriter(file, true);
+        FileWriter out1 = new FileWriter(file1, true);
 
 
-        for (int i = 1060228; i < 2057900; i++) {
+        for (int i = 2061989 ; i < 3057900; i++) {
             try {
                 Document document = Jsoup.connect("http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/" + Integer.toString(i)).get();
                 if ("链接可能失效或不存在，请重新获取。".equals(document.getElementsByTag("title").text())) {
+                } else if ("店铺未审核".equals(document.getElementsByTag("title").text())){
+                    out1.write("http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/"
+                            + Integer.toString(i) + "\n");
+                    System.out.println("未审核 http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/" + Integer.toString(i));
+                    out1.flush();
                 } else {
-
                     out.write("http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/"
                             + Integer.toString(i) + "\n");
                     System.out.println("http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/" + Integer.toString(i));
@@ -37,5 +43,6 @@ public class URLTest {
 
         }
         out.close();
+        out1.close();
     }
 }
