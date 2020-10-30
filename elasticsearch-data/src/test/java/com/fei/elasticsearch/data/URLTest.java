7784072f8832047c6cd1c665c09336f57bc70472
chengfei <xingchendahai_fei@163.com>
2020-10-30 22:14:29 +0800

    爬取网站数据

--- elasticsearch-data/src/test/java/com/fei/elasticsearch/data/URLTest.java
+++ elasticsearch-data/src/test/java/com/fei/elasticsearch/data/URLTest.java
@@ -2,6 +2,7 @@ package com.fei.elasticsearch.data;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import sun.java2d.pipe.SpanIterator;
 
 import java.io.File;
@@ -19,17 +20,37 @@ public class URLTest {
         File file = new File("C:\\movie\\movie.txt");
         FileWriter out = new FileWriter(file, true);
 
+        File file1 = new File("C:\\movie\\not_movie.txt");
+        FileWriter out1 = new FileWriter(file1, true);
 
-        for (int i = 1060228; i < 2057900; i++) {
+        File file2 = new File("C:\\movie\\happy_movie.txt");
+        FileWriter out2 = new FileWriter(file2, true);
+
+
+        for (int i = 1226954; i < 2057900; i++) {
             try {
                 Document document = Jsoup.connect("http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/" + Integer.toString(i)).get();
                 if ("链接可能失效或不存在，请重新获取。".equals(document.getElementsByTag("title").text())) {
-                } else {
-
-                    out.write("http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/"
+                }else if ("店铺未审核".equals(document.getElementsByTag("title").text())){
+                    out1.write("http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/"
                             + Integer.toString(i) + "\n");
-                    System.out.println("http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/" + Integer.toString(i));
-                    out.flush();
+                    System.out.println("店铺未审核  http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/" + Integer.toString(i));
+                    out1.flush();
+                }else {
+                    Elements good = document.getElementsByClass("good");
+                    if (good.size() >= 1){
+                        out2.write("http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/"
+                                + Integer.toString(i) + "\n");
+                        System.out.println("高质量往网站：http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/" + Integer.toString(i));
+                        out2.flush();
+                    }else {
+
+                        out1.write("http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/"
+                                + Integer.toString(i) + "\n");
+                        System.out.println("http://fk.gofaka.cn/fkpt/index.php/Home/index/index/uid/" + Integer.toString(i));
+                        out1.flush();
+                    }
+
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -37,5 +58,6 @@ public class URLTest {
 
         }
         out.close();
+        out1.close();
     }
 }
