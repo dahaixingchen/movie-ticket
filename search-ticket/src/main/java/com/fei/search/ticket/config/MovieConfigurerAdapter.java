a1f421ea1164f68d16eba708e12f77ac9bc76369
程飞 <chengfei@newland.com.cn>
2020-11-10 15:09:51 +0800

    MVC中的首页访问

---
+++ search-ticket/src/main/java/com/fei/search/ticket/config/MovieConfigurerAdapter.java
@@ -0,0 +1,23 @@
+package com.fei.search.ticket.config;
+
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @ClassName: MovieConfigurerAdapter
+ * @Author chengfei
+ * @Date 2020/11/10 14:52
+ * @Description: TODO
+ **/
+@Component
+public class MovieConfigurerAdapter implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/login.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        WebMvcConfigurer.super.addViewControllers(registry);
+    }
+}
